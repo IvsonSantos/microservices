0b77f40f01c06ce6cecd8f06b692a4d82ce16dca
ivsonsantos <ivson.java@gmail.com>
2023-09-05 21:32:00 +0200

    Projeto criado

---
+++ src/main/java/com/webservices/webservices/controller/HelloBean.java
@@ -0,0 +1,17 @@
+package com.webservices.webservices.controller;
+
+public class HelloBean {
+
+    private String message;
+    public HelloBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/webservices/webservices/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.webservices.webservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "Hello world";
+    }
+
+    @GetMapping(value = "/helloBean")
+    public HelloBean helloBean() {
+        return new HelloBean("Hello Bean");
+    }
+
+    @GetMapping(path = "/hello/{name}")
+    public HelloBean helloPath(@PathVariable String name) {
+        return new HelloBean(String.format("Hello %s ", name));
+    }
+}
