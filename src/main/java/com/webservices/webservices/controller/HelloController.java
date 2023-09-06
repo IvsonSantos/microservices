a5a7f93acd0033517fa7dcc48d0ceb592273a67a
ivsonsantos <ivson.java@gmail.com>
2023-09-06 22:06:37 +0200

    Internacionalização

--- src/main/java/com/webservices/webservices/controller/HelloController.java
+++ src/main/java/com/webservices/webservices/controller/HelloController.java
@@ -1,11 +1,19 @@
 package com.webservices.webservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(value = "/hello")
     public String hello() {
@@ -20,6 +28,12 @@ public class HelloController {
     @GetMapping(path = "/hello/{name}")
     public HelloBean helloPath(@PathVariable String name) {
         return new HelloBean(String.format("Hello %s ", name));
+    }
+
+    @GetMapping(path = "/hello-inter")
+    public String helloPath() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning", null, "default", locale);
     }
 
 }
--- src/main/java/com/webservices/webservices/controller/UserController.java
+++ src/main/java/com/webservices/webservices/controller/UserController.java
@@ -3,8 +3,11 @@ package com.webservices.webservices.controller;
 import com.webservices.webservices.model.User;
 import com.webservices.webservices.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,12 +25,26 @@ public class UserController {
     // GET /users
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public User delete(@PathVariable int id) {
+        User user = service.findOne(id);
+        return user;
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
         User novo = service.save(user);
-        return novo;
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(novo.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
